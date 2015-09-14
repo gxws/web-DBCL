b88da59a0a8369f9f1dfbd4242fd2bbb96863bbe
jqjjian-pc <jqjjian@126.com>
2015-09-14 16:32:05 +0800

    :lollipop: Added .gitattributes

---
+++ static/css.js
@@ -0,0 +1,13 @@
+return {
+	files: {
+		/* begin 基础模块 */
+		//0001机顶盒
+		'{static}/web-DBCL/static/css/base.mod.css': [
+			'{static}/web-DBCL/static/css/base.min.css',
+			'{static}/web-DBCL/static/css/base_HD.min.css',
+		],
+		//0002&0003机顶盒
+		
+		/* end 基础模块 */
+	}
+}
